b5ce53dcf457899a89532c9b99f76dd1399d3a01
Zach Banducci <zbducci@gmail.com>
2019-03-05 12:06:36 -0600

    Added contact form component and utilized some react hooks to manage state

--- interviews/javascript/frontend/jamstack/src/components/contact-form.js
+++ interviews/javascript/frontend/jamstack/src/components/contact-form.js
@@ -3,10 +3,35 @@ import React, { useState } from 'react'
 const ContactForm = () => {
   const [firstName, setFirstName] = useState("")
   const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
   const [additionalInfo, setAdditionalInfo] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(firstName, lastName, email, additionalInfo)
+  }
+
   return(
-    <form>
-      
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" id="firstName" onChange={e => setFirstName(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" id="lastName" onChange={e => setLastName(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" onChange={e => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="addtionalInfo">Additional Info</label>
+        <input type="text" id="addtionalInfo" onChange={e => setAdditionalInfo(e.target.value)} />
+      </div>
+      <button type="submit">Contact JamStack</button>
     </form>
   )
 }
+
+export default ContactForm
