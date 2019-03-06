d0032bdfab0bef3e3a209b06a0ec37486fb9b690
Zach Banducci <zbducci@gmail.com>
2019-03-06 00:29:28 -0600

    Edited layout styling

--- interviews/javascript/frontend/jamstack/src/components/layout.js
+++ interviews/javascript/frontend/jamstack/src/components/layout.js
@@ -26,8 +26,8 @@ const Layout = ({ children }) => (
         <div
           style={{
             margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            maxWidth: 800,
+            padding: `0px 1.0875rem`,
             paddingTop: 0,
           }}
         >
