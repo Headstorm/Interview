a01fa6fa127d20e63e3232ad49f03fafc78d7821
Zach Banducci <zbducci@gmail.com>
2019-03-06 17:20:46 -0600

    Cleaned up boilerplate in layout

--- interviews/javascript/frontend/jamstack/src/components/layout.js
+++ interviews/javascript/frontend/jamstack/src/components/layout.js
@@ -1,48 +1,19 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 import "./normalize.css"
 import "./skeleton.css"
 import "./layout.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 800,
-            padding: `0px 1.0875rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+  <div className="content">
+    <main>{children}</main>
+    <footer>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>,
+      engineered by Zach Banducci
+    </footer>
+  </div>
 )
 
 Layout.propTypes = {
