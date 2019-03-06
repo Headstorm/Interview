316e996b43042264b40ff79ac659091fea31c9dd
Zach Banducci <zbducci@gmail.com>
2019-03-06 00:30:53 -0600

    Added additional html and styling content in index

--- interviews/javascript/frontend/jamstack/src/pages/index.js
+++ interviews/javascript/frontend/jamstack/src/pages/index.js
@@ -3,20 +3,32 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ContactForm from "../components/contact-form"
+import "./index.css"
+import Logo from "../components/logo"
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <h1>Jamstack</h1>
-      <h4>Democratize the aux</h4>
-      <h2>A new way to listen to music</h2>
-      <p>
-        Listening to music is a social experience, so why not make it more
-        collaborative? JamStack allows you to create collaborative queues with
-        Spotify
-      </p>
-      <ContactForm />
+      <div style={{display: "flex", justifyContent: "space-between"}}>
+        <div style={{ width: "400px" }}>
+          <Logo />
+        </div>
+        <div className="banner">
+            <h1>Jamstack</h1>
+            <h4>Democratize the aux</h4>
+        </div>
+      </div>
+      <div>
+        <h2>A new way to listen to music</h2>
+        <p>
+          Listening to music is a social experience, so why not make it more
+          collaborative? JamStack allows you to create collaborative queues with
+          Spotify
+        </p>
+        <h3>Contact the JamStack Crew</h3>
+        <ContactForm />
+      </div>
     </Layout>
   )
 }
