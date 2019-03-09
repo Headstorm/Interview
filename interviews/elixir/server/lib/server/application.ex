ba031fcba248acb50cde913450ea42e4d43fadf9
Zach Banducci <zbducci@gmail.com>
2019-03-08 23:27:38 -0800

    Removed unnecessary sql and ecto stuff

--- interviews/elixir/server/lib/server/application.ex
+++ interviews/elixir/server/lib/server/application.ex
@@ -8,8 +8,6 @@ defmodule Server.Application do
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
-      supervisor(Server.Repo, []),
       # Start the endpoint when the application starts
       supervisor(ServerWeb.Endpoint, []),
       # Start your own worker by calling: Server.Worker.start_link(arg1, arg2, arg3)
--- interviews/elixir/server/mix.exs
+++ interviews/elixir/server/mix.exs
@@ -35,10 +35,10 @@ defmodule Server.Mixfile do
     [
       {:phoenix, "~> 1.3.4"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
+      {:poison, "~> 3.1.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"}
     ]
   end
 
