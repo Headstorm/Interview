a965d817e4f4097836170215a878f6fa07248396
Zach Banducci <zbducci@gmail.com>
2019-03-08 19:43:56 -0600

    Added initial paths

--- interviews/elixir/server/lib/server_web/router.ex
+++ interviews/elixir/server/lib/server_web/router.ex
@@ -7,5 +7,9 @@ defmodule ServerWeb.Router do
 
   scope "/api", ServerWeb do
     pipe_through :api
+
+    get "/data", DataController, :index
+    post "/data", DataController, :create
+    patch "/data", DataController, :update
   end
 end
