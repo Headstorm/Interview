50ff254e4ce58ea72f82096015f332c5b1b04b3d
Zach Banducci <zbducci@gmail.com>
2019-03-11 12:16:34 -0700

    Added NumberStorage module in app supervisor

--- interviews/elixir/server/lib/server/application.ex
+++ interviews/elixir/server/lib/server/application.ex
@@ -10,6 +10,7 @@ defmodule Server.Application do
     children = [
       # Start the endpoint when the application starts
       supervisor(ServerWeb.Endpoint, []),
+      supervisor(Server.NumberStorage, [])
       # Start your own worker by calling: Server.Worker.start_link(arg1, arg2, arg3)
       # worker(Server.Worker, [arg1, arg2, arg3]),
     ]
