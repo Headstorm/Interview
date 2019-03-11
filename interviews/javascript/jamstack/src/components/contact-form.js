245d57ecaff50e2e6509673c8dd6b94b4e1e5524
Zach Banducci <zbducci@gmail.com>
2019-03-11 16:26:06 -0700

    Changed secret configuration in contact-form

--- interviews/javascript/jamstack/src/components/contact-form.js
+++ interviews/javascript/jamstack/src/components/contact-form.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import Helmet from "react-helmet"
 import { ReCaptcha } from "recaptcha-v3-react"
 import './contact-form.css'
-import { SECRET_KEY } from "../config.js"
+import config from "../config.js"
 
 const ContactForm = () => {
   const [captchaToken, setCaptchaToken] = useState("")
@@ -27,13 +27,14 @@ const ContactForm = () => {
   }
 
   const verifyCaptcha = async (token) => {
-   let req = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
+    let req = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: `secret=${SECRET_KEY}&response=${token}`
+      body: `secret=${config.SECRET_KEY}&response=${token}`
     })
 
     let resp = await req.json()
+
     return resp
   }
 
