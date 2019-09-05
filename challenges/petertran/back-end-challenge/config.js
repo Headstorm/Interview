84f13271d00a715e80ecebcb20048b9d2751dbab
Peter Tran <tran.peter08@gmail.com>
2019-09-05 14:42:34 -0700

    created .env variables

---
+++ challenges/petertran/back-end-challenge/config.js
@@ -0,0 +1,3 @@
+exports.RECAPTCHA_SECRET = process.env.RECAPTCHA_SECRET;
+exports.DB_URL = process.env.DB_URL || 'mongodb://localhost/headstorm-challenge';
+exports.PORT = process.env.PORT || '8080';
