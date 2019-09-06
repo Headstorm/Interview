fa6f0425b8d9771eb41426b44c6db727e3a60cf3
Peter Tran <tran.peter08@gmail.com>
2019-09-05 22:43:55 -0700

    create node server to handle merging database

---
+++ challenges/petertran/database-challenege/part2/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const mysql = require('mysql');
+
+const upload = multer({dest: 'uploads/'});
+const app = express();
+
+app.use(express.json());
+
+app.post('/mergeDB', upload.single('noSQL'),  (req, res) => {
+  const jsonData = fs.readFileSync(req.file.path, {encoding: 'utf8'});
+  const data = JSON.parse(jsonData);
+  const values = [];
+
+  for (let i = 0; i < data.length; i++) {
+    const row = [];
+    for (let key in data[i]) {
+      row.push(data[i][key]);
+    }
+    values.push(`(${row.join(',')})`);
+  }
+
+  const insertInto = `
+    INSERT INTO newDB 
+      (
+      RecordID,
+      Name,
+      CellPhone,
+      WorkPhone,
+      Email,
+      Address,
+      BasicWidgetOrder,
+      AdvanceWidgetOrder,
+      ProtectionPlan
+      )
+  
+    VALUES (${values.join(',')})
+  `;
+
+  fs.unlinkSync(req.file.path);
+  res.status(200).end();
+});
+
+app.listen('8080', () => {
+  console.log('App is listening on port: 8080');
+});
