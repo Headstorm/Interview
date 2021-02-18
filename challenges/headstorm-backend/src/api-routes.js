dcb20153c52fecff6a69ed56e51ba8bc54a4fad4
Grandelisn <grandelisn@gmail.com>
2021-02-18 18:44:32 -0500

    final backend

--- challenges/headstorm-backend/src/api-routes.js
+++ challenges/headstorm-backend/src/api-routes.js
@@ -1,18 +1,21 @@
+//declares router as middleware to modularize api requests
 let router = require( 'express' ).Router();
 
-const projectController = require( './projectController' );
+//imports a custom controller for utilizing data 
+const dataController = require( './dataController' );
 
+//sets default api route to show a success message
 router.get( '/', function( req, res ) {
     res.json({
-        status: 'API Is Working bitch, Im sorry I didnt mean that',
+        status: 'API Is Working',
         message: 'Welcome to the API'
     })
 })
-
+//defines a /data route
 router.route( '/data' )
-    .get( projectController.index )
-    .post( projectController.new )
-
+    .get( dataController.index )
+    .post( dataController.new )
+    .patch(dataController.patch)
 
 
 module.exports = router;
--- challenges/headstorm-backend/src/index.js
+++ challenges/headstorm-backend/src/index.js
@@ -1,36 +1,39 @@
+//imports
 let express = require( 'express' );
 let bodyParser = require( 'body-parser' );
 let mongoose = require( 'mongoose' );
 let apiRoutes = require( './api-routes' );
-let cors = require('cors');
-let {response} = require('express');
 
 
+//creating an express application called app
 let app = express();
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", '*');
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-//   });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
+  //get the http requests and formats it to json
 app.use( bodyParser.json() );
 
+//opens the default connection to mongodb and sets it to a localhost test server
 mongoose.connect( 'mongodb://localhost/testServer', { useNewUrlParser: true } );
 
+//setting the connection to a variable, then checking to see if there is a connection
 const db = mongoose.connection;
-
 if( !db ){
     console.log( 'error connecting to database' );
 }else{
     console.log( 'Database connected successfully' );
 }
 
+//declares port as the environment PORT variable or if nothing exists 3001
 const port = process.env.PORT || 3001;
 
+//configuring routes
 app.get( '/', ( req, res ) => res.send( 'Hello' ) );
-
 app.use( '/api', apiRoutes );
 
 
