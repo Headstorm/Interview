e0caff404ab5002d752104edf66812778b93f406
Travis A. Dula <travisadula@gmail.com>
2021-03-16 17:27:30 -0500

    GET and POST working

--- challenges/backend/backend.py
+++ challenges/backend/backend.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+data_list = []
+
+@app.route('/data', methods=['GET', 'POST', 'PATCH'])
+def data_api():
+    global data_list
+    if request.method == 'GET':
+        return jsonify(data_list)
+    elif request.method == 'POST':
+        in_json = request.get_json()
+        if len(in_json) == 500 and all(map(lambda x: type(x) == int, in_json)):
+            data_list = list(sorted(in_json))
+            return jsonify(data_list)
+        else:
+            return 'Bad request', 400
+    elif request.method == 'PATCH':
+        pass
 
 if __name__ == '__main__':
     app.run(debug=True)
