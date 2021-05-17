deb03e218f6cb86c164eacff9d74840e99fbb96b
Davidazael <davidazaelbernal@gmail.com>
2021-05-17 10:31:03 -0500

    Modified Models to have address model, and added a View Creation that will create a view to visualize data. I am sure I didn't cover everything that is needed.

--- challenges/database-challenge/Models.py
+++ challenges/database-challenge/Models.py
@@ -1,5 +1,15 @@
 
 class Record:
+    '''
+    This is the Records Model.
+
+    Functionality:
+        Records are suppose to store the transaction details, for each customer.
+
+    Returns:
+        Record that contains the details and the Customers Reference Key.
+
+    '''
 
     def __init__(self, record_id: int, basic_widget_order: int, advanced_widget_order: int, protection_plan: bool = False, customer_id: int = None) -> None:
         self.record_id = record_id
@@ -8,8 +18,14 @@ class Record:
         self.advanced_widget_quantity = advanced_widget_order
         self.protection_plan = protection_plan
 
+    def __repr__(self) -> str:
+        return ('Record: {0.record_id} | {0.customer_id} | {0.basic_widget_quantity} | {0.advanced_widget_quantity} | {0.protection_plan}'.format(self))
+
 
 class User:
+    '''
+    User Model. Used to store User data.
+    '''
     _UserID = 1
 
     def __init__(self, name: str, cell_number: str, work_number: str, email: str, address: str):
@@ -19,7 +35,47 @@ class User:
         self.cell_num = cell_number
         self.work_num = work_number
         self.email = email
-        self.address = address
+        self._Address = address
+
+    @property
+    def Address(self):
+        return self._Address
+
+    @Address.setter
+    def Address(self, value):
+        address = Address()
+        self._Address = address.distribute(value)
 
     def __repr__(self) -> str:
-        return (' Name: {0.name!r} | Cell#: {0.cell_num!r} | Work#: {0.work_num!r} | Email: {0.email!r} | Address: {0.address!r} '.format(self))
+        return (' Name: {0.name!r} | Cell#: {0.cell_num!r} | Work#: {0.work_num!r} | Email: {0.email!r} | Address: {0.Address}'.format(self))
+
+
+class Address():
+    '''
+    Address Model that should be used to store addresses. To prevent duplication.
+    '''
+
+    def __init__(self):
+        self.street_address = ''
+        self.state = ''
+        self.city = ''
+        self.zip_code = ''
+
+    @property
+    def street_address(self) -> str:
+        return self._street_address
+
+    @street_address.setter
+    def street_address(self, value):
+        self._street_address = str(value)
+
+    def distribute(self, full_address):
+        partitioned = full_address.split(', ')
+        self.street_address = partitioned[0]
+        addr_specifics = partitioned[1].split(' ')
+        self.city = addr_specifics[0]
+        self.state = addr_specifics[1]
+        self.zip_code = addr_specifics[2]
+
+    def __repr__(self) -> str:
+        return ('{0.street_address}, {0.city} {0.state} {0.zip_code}'.format(self))
--- challenges/database-challenge/app.py
+++ challenges/database-challenge/app.py
@@ -1,9 +1,6 @@
 import os
-import sys
-import random
 import json
 from Models import User, Record
-from pprint import pp
 
 path = os.getcwd()
 
@@ -34,8 +31,11 @@ def format_unormalized_data():
         given_record_print_query(rec)
         given_user_print_query(user)
 
+    create_record_view(Record, User)
+
 
 def given_record_print_query(rec: Record):
+    # Inserts Record into record entity
     table_name = 'RECORDS'
     query = "INSERT INTO {0} ( {1.record_id}, {1.customer_id}, {1.basic_widget_quantity}, {1.advanced_widget_quantity}, {1.protection_plan} )".format(
         table_name, rec)
@@ -44,10 +44,21 @@ def given_record_print_query(rec: Record):
 
 
 def given_user_print_query(user: User):
+    # Inserts User into Users Entity
     table_name = 'CUSTOMERS'
-    query = "INSERT INTO {0} ( {1.name}, {1.cell_num}, {1.work_num}, {1.email}, {1.address} )".format(
+    query = "INSERT INTO {0} ( {1.name}, {1.cell_num}, {1.work_num}, {1.email}, {1.Address} )".format(
         table_name, user)
 
+    print(query, end="\n")
+
+
+def create_record_view(rec: Record, user: User):
+    '''
+    Creates a view that will display records like in the JSON data given.
+    '''
+    table_name = 'RECORD_VIEW'
+    create_view = 'CREATE VIEW {0} AS SELECT'.format(table_name)
+    query = '{0} {1} {2}'.format(create_view, rec, user)
     print(query, end="\n")
 
 
