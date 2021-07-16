84bc3c6154f8dbe83403e60c129c0b3f966a09cd
ecsw98 <ecsw98@umsystem.edu>
2021-07-15 20:37:22 -0500

    Submission for 3 challenges

---
+++ challenges/backend/server.py
@@ -0,0 +1,51 @@
+from flask import Flask, request
+from flask_restful import Resource,Api,reqparse, abort
+import requests
+
+app = Flask(__name__)
+api = Api(app)
+
+NUMBERLIST = {"list":[]}
+
+class ListProcess(Resource):
+	def get(self):
+		#Sort before returning
+		NUMBERLIST['list'].sort()
+		return NUMBERLIST
+	
+	def post(self):
+		numlist = str(request.data).strip()
+		numlist = numlist.split(sep=',')
+		templist = []
+		if(len(numlist) == 500):
+			allnumbers = True
+			for entry in numlist:
+				entry = entry.replace('b\'','')
+				entry = entry.replace('\'','')
+				if(entry.isdigit() == False):
+					allnumbers = False
+				else:
+					templist.append(int(entry))
+			if(allnumbers == True):
+				NUMBERLIST['list'] = templist
+				return NUMBERLIST
+			else:
+				abort(400,message="Bad input. All entries must be numbers.")
+		else:
+			abort(400, message="Bad input. Length must be 500.")
+	
+	def patch(self):
+		entry = str(request.data).strip()
+		entry = entry.replace('b\'', '')
+		entry = entry.replace('\'', '')
+		if(entry.isdigit()):
+			NUMBERLIST['list'].append(int(entry))
+			NUMBERLIST['list'].sort()
+			return NUMBERLIST
+		else:
+			abort(400,message="Bad input. All entries must be numbers.")
+
+api.add_resource(ListProcess, "/data/",endpoint='data')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ challenges/backend/tester.py
@@ -0,0 +1,14 @@
+import requests
+import random
+payload = ''
+for i in range(499):
+	temp = random.randint(0,500)
+	payload = payload + str(temp) + ','
+payload = payload + str(random.randint(0,500))
+patchload = str(random.randint(0,500))
+r=requests.post("http://localhost:5000/data/",data=payload)
+print("post",r.text)
+r=requests.get("http://localhost:5000/data/")
+print("get", r.text)
+r=requests.patch("http://localhost:5000/data/",data=patchload)
+print("patch", r.text)
