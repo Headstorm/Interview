c3f27ad0ca5609f7a3867ecea1dea682c4c0bfe9
Emily Miller <emm190@pitt.edu>
2021-09-30 15:48:35 -0400

    Finished front end challenge

---
+++ HFrontEndChallenge/public/js/script.js
@@ -0,0 +1,106 @@
+function onSubmit(token) { 
+    console.log("submitted called"); 
+    $('#comment_form').submit(); 
+    console.log("Submitted" + token);
+    //Very important line, it disable the page refresh.
+    return false;
+}
+$(document).ready(function() {
+   console.log("Ready");
+  $('#comment_form').submit(function() {
+    $(this).ajaxSubmit({
+      error: function(xhr) {
+        console.log("Error"); 
+      },
+     success: function(response) {
+      try {
+        var parsed_data = JSON.parse(response.responseCode);
+        if (parsed_data==1)
+        {
+          console.log("Recaptcha failed, alert user & return"); 
+          alert("Recaptcha failed, try again :(")
+          return;
+        }
+      } catch (error) {
+        console.log("responseCode is undefined, must have passed");
+      }
+      //if it made it to here, it passed the recaptcha check so display info 
+      var name = document.getElementById("name");
+      var email = document.getElementById("email");
+      var phoneNumber = document.getElementById("phoneNumber");
+      var questions = document.getElementById("questions");
+      console.log("Form submission: ------------------");
+      console.log("Name: " + name.value);
+      console.log("Email: " + email.value);
+      console.log("Phone Number: " + phoneNumber.value);
+      console.log("Questions: " + questions.value);
+      console.log("End form submission: ---------------");
+      console.log("The recaptcha worked, the response is below: ");
+      console.log(response); 
+      isSubmitted=true; 
+      //switch form to close form 
+      toggleOnSubmit(); 
+     }
+    });
+    //disables refresh
+    return false;
+  });
+});
+
+var isSubmitted = false;
+console.log("Being read"); 
+function sConsole(event) {
+    event.preventDefault();
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var phoneNumber = document.getElementById("phoneNumber");
+    var questions = document.getElementById("questions");
+    console.log("Form submission: ------------------");
+    console.log("Name: " + name.value);
+    console.log("Email: " + email.value);
+    console.log("Phone Number: " + phoneNumber.value);
+    console.log("Questions: " + questions.value);
+    console.log("End form submission: ---------------");
+    if (name.value!="") {
+        document.getElementById("form").innerHTML="Thank you, " + name.value; 
+        document.getElementById("form").reset();
+    }
+    else
+        document.getElementById("form").innerHTML="Thank you!"; 
+    
+    isSubmitted=true; 
+    toggleOnSubmit(); 
+}
+
+function toggleOnSubmit() { 
+    if (isSubmitted==false) {
+    document.getElementById("comment_form").innerHTML=
+            `<div class="form-row">
+                  <label id="namelabel" for="name">Name</label>
+                  <input type="text" class="form-control" id="name" placeholder="Emily Miller">
+              </div>
+              <div class="form-row">
+                <label id="emaillabel" for="inputEmail4">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="miller24emily@gmail.com">
+              </div>
+              <div class="form-row">
+                <label id="phonenumberlabel" for="phoneNumber">Phone Number (form XXX-XXX-XXXX)</label>
+                <input type="tel" class="form-control" id="phoneNumber" placeholder="330-987-0225">
+              </div>
+              <div class="form-row">
+                <label id="questionlabel" for="questions">Questions</label>
+                <input type="text" class="form-control" id="questions" placeholder="Any questions? How can we help you!">
+              </div>
+              <br>
+              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>`;
+              isSubmitted=true;
+    }
+
+    else {
+    document.getElementById("comment_form").innerHTML=
+        `Thank you! We will be in touch soon. <br><br> <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick="window.location.reload();">Close</button>`;
+    isSubmitted=false;
+    }
+    
+}
---
+++ HFrontEndChallenge/views/verify.php
@@ -0,0 +1,16 @@
+<?php
+  require_once('recaptchalib.php');
+  $privatekey = "your_private_key";
+  $resp = recaptcha_check_answer ($privatekey,
+                                $_SERVER["REMOTE_ADDR"],
+                                $_POST["recaptcha_challenge_field"],
+                                $_POST["recaptcha_response_field"]);
+
+  if (!$resp->is_valid) {
+    // What happens when the CAPTCHA was entered incorrectly
+    die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
+         "(reCAPTCHA said: " . $resp->error . ")");
+  } else {
+    // Your code here to handle a successful verification
+  }
+  ?>
