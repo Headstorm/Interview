44efc3d48bacbeea353bb4ea8ca35909755d82e2
Tony-Sommer <65473636+Tony-Sommer@users.noreply.github.com>
2021-10-11 16:26:55 -0500

    Only Important Files
    Made a folder for each challenge. Front End is mostly Index.cshtml.

---
+++ Back End Challenge/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Newtonsoft.Json;
+
+namespace Headstorm_Front_End_Challenge
+{
+    public class HomeController : ApiController
+    {
+        //Used double since it didn't specify that numbers would be integers. 
+        List<double> StoredList = new List<double>();
+        //Didn't want to deal with permissions so just stored it on a spared drive I have.
+        string fileName = "E:\\test.txt";
+
+
+        [HttpPost]
+        public bool GetNumberList([FromBody] List<string> list)
+        {
+            //false is error.
+            if (list.Count != 500)
+            {
+                return false;
+            }
+
+            foreach (string item in list)
+            {
+                double temp;
+                if (Double.TryParse(item, out temp))
+                {
+                    StoredList.Add(temp);
+                }
+                else
+                {
+                    //false is error.
+                    return false;
+                }
+            }
+
+            StoredList.Sort(); //From the wording it implies the list should be sorted at this point.
+
+            //In the real world I'd put the file IO in separate functions, but didn't want to add a bunch of files to this project.
+            File.Delete(fileName);
+            TextWriter tw = new StreamWriter(@fileName, true);
+
+            tw.WriteLine(JsonConvert.SerializeObject(StoredList));
+
+            tw.Flush();
+            tw.Close();
+
+            return true;
+        }
+
+        [HttpGet]
+        public string SendNumberList()
+        {
+            if (File.Exists(fileName))
+            {
+                String JSONtxt = File.ReadAllText(fileName);
+                StoredList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<double>>(JSONtxt);
+            }
+
+            return JsonConvert.SerializeObject(StoredList);
+        }
+
+        [HttpPatch]
+        public bool ModifyNumberList([FromBody] JsonPatchDocument patchDoc)
+        {
+            if (File.Exists(fileName))
+            {
+                String JSONtxt = File.ReadAllText(fileName);
+                StoredList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<double>>(JSONtxt);
+            }
+            else
+            {
+                return false;
+            }
+
+            try
+            {
+                if (patchDoc != null)
+                {
+                    patchDoc.ApplyTo(StoredList);
+                }
+
+                //The instructions said to put the value in the appropriate location in the list, so I figure sorting it will get everything in order again if they place it in the wrong spot.
+                StoredList.Sort();
+                //Instructions unclear if the list should grow or remain at 500, but if it should remain at 500 then remove the last element from the list here.
+
+                File.Delete(fileName);
+                TextWriter tw = new StreamWriter(@fileName, true);
+
+                tw.WriteLine(JsonConvert.SerializeObject(StoredList));
+
+                tw.Flush();
+                tw.Close();
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+        }
+
+        
+    }
+}
---
+++ Database Challenge/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Headstorm_Database_Challenge
+{
+
+    public class Record
+    {
+        public int RecordID { get; set; }
+        public string Name { get; set; }
+        public string CellPhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int? BasicWidgetOrder { get; set; }
+        public int? AdvancedWidgetOrder { get; set; }
+        public bool ProtectionPlan { get; set; }
+    }
+
+
+    class Program
+    {
+        public static string SQLQueryMaker()
+        {
+            StringBuilder result = new StringBuilder();
+            List<Record> records = new List<Record>();
+            string fileName = System.Environment.CurrentDirectory + "\\DatabaseRecords.json";
+
+            if (File.Exists(fileName))
+            {
+                String JSONtxt = File.ReadAllText(fileName);
+                records = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Record>>(JSONtxt);
+            }
+
+            result.AppendLine("DECLARE @CustomerID AS INT");
+            result.AppendLine("");
+
+            foreach (Record record in records)
+            {
+                //Insert the customer into the Customers table if it's a new customer, otherwise get the existing customer ID to link the order.
+                //Usually would use parameters instead of adding strings, but that doesn't work when printing a query to a file.
+                //I haven't tested the SQL statements themselves, but they should either work or be close to working.
+                result.AppendLine("IF " + record.Email + " IN (SELECT Email FROM Customers)");
+                result.AppendLine("BEGIN");
+                result.AppendLine("SELECT @CustomerID = CustomerID FROM Customers WHERE Email = " + record.Email);
+                result.AppendLine("END");
+                result.AppendLine("ELSE");
+                result.AppendLine("BEGIN");
+                result.AppendLine("INSERT INTO Orders (Name, CellPhone, WorkPhone, Email, Address)");
+                result.AppendLine("OUTPUT Inserted.ID INTO @CustomerID");
+                result.AppendLine("VALUES( " + record.Name + ", " + record.CellPhone + ", " + record.WorkPhone + ", " + record.Email + ", " + record.Address + " )");
+                result.AppendLine("END");
+                result.AppendLine("");
+                result.AppendLine("INSERT INTO Orders (CustomerID, RecordID, WidgetOrder, Advanced, ProtectionPlan) VALUES (@CustomerID, " + record.RecordID + ", " + (record.BasicWidgetOrder == null ? record.AdvancedWidgetOrder : record.BasicWidgetOrder) + ", " + (record.BasicWidgetOrder == null ? 1 : 0) + ", " + (record.ProtectionPlan ? 1 : 0 ) + " )");
+                result.AppendLine("");
+            }
+
+            return result.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string fileName = System.Environment.CurrentDirectory + "\\test.txt";
+            File.Delete(fileName);
+            TextWriter tw = new StreamWriter(@fileName, true);
+
+            tw.Write(SQLQueryMaker());
+
+            tw.Flush();
+            tw.Close();
+        }
+    }
+}
